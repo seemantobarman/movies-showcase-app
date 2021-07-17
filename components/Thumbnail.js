2edae0c5ad74e05d1e3f564c5f15e421507aa511
Seemanto Barman <seemanto@hotmail.com>
2021-07-18 00:41:41 +0600

    thumbnail fix

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -11,6 +11,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
             className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
         >
             <Image
+                alt="Thumbnail Image"
                 layout="responsive"
                 src={
                     `${BASR_URL}${
